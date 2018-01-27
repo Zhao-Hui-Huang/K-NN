906fb3a29fdb252726550e2a7149d3788998ae8c
hzh <hzh@mail.imu.edu.cn>
2018-01-27 16:06:21 +0800

    add hardwriting class

--- kNN.py
+++ kNN.py
@@ -1,3 +1,5 @@
+from os import listdir
+
 from numpy import *
 import operator
 import matplotlib.pyplot as plt
@@ -78,8 +80,55 @@ def autoNorm(dataSet):
     return normDataSet
 
 
+def img2vector(filename):
+    """
+    将32*32图像数据转为1*1024
+    :param filename:
+    :return:
+    """
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    """
+    手写数字识别系统
+    :return:
+    """
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')  # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print
+        "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
+
+
 def main():
-    datingClassTest()
+    handwritingClassTest()
 
 
 if __name__ == '__main__':
